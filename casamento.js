ba79a54e6a8341823467fa540a6cfd14e9ff5801
daniele ferrari <eleinadinha@hotmail.com>
2017-03-23 14:50:56 -0300

    comecando o mobile

---
+++ casamento.js
@@ -0,0 +1,19 @@
+var aux = true;
+
+function abrir_menu (){
+
+	if (aux == true) { 
+		document.getElementById("menu_mobile").style = "display:block";
+		aux = false;
+	}
+	else
+	{
+		document.getElementById("menu_mobile").style = "display:none";
+		aux = true;
+	}
+	
+}
+
+function fechar_menu (){
+	document.getElementById("menu_mobile").style = "display: none;"
+}
